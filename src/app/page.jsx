fe6c9d761b686489a7b89e0e10114d5192b3d9db
Henry Agustin <henryagustin297@gmail.com>
2023-09-24 10:56:11 -0600

    seccion medical deptos completada

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,7 @@ import ContactBanner from './components/ContactBanner';
 import Services from './components/Services';
 import AboutHospital from './components/AboutHospital';
 import LatestNews from './components/LatestNews';
+import MedicalDep from './components/MedicalDep';
 
 const HomePage = () => {
   return (
@@ -18,6 +19,7 @@ const HomePage = () => {
         <Services/>
         <AboutHospital/>
         <LatestNews/>
+        <MedicalDep/>
       </div>
     </main>
   )
